5c9aca9be5724fbc69a36bace177f1e70332625f
aaa <93630749+Aron113@users.noreply.github.com>
2024-04-21 23:26:44 +0800

    Update stscraping_insertion.py

--- dags/stscraping_insertion.py
+++ dags/stscraping_insertion.py
@@ -23,7 +23,7 @@ default_args = {
 credentials = service_account.Credentials.from_service_account_file('IS3107 Keys.json')
 client = bigquery.Client(credentials=credentials)
             
-@dag(dag_id='stscraping_insertion', default_args=default_args, schedule=None, catchup=False, tags=['IS3107_Project'])
+@dag(dag_id='stscraping_insertion', default_args=default_args, schedule_interval='@daily', catchup=False, tags=['IS3107_Project'])
 def project():
 
     @task
